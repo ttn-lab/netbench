5a9ad48f4d7e4c0b293709cf897a953597c9fef7
Víctor Vázquez <victorvazrod@ugr.es>
2020-12-02 13:43:53 +0100

    Elimina dependencia del paquete iperf3

--- setup.py
+++ setup.py
@@ -23,8 +23,7 @@ setup(
     include_package_data=True,
     install_requires=[
         'click',
-        'pandas',
-        'iperf3'
+        'pandas'
     ],
     entry_points='''
         [console_scripts]
